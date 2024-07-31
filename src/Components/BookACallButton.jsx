84070bc154840a794341c2100d7a5f4df8889775
Sania Malik <sania.malik147258@gmail.com>
2024-07-31 12:53:43 +0500

    31July

--- src/Components/BookACallButton.jsx
+++ src/Components/BookACallButton.jsx
@@ -11,19 +11,13 @@ const BookACallButton = () => {
   }, []);
 
   return (
-    <div class="relative" >
-    <span class="absolute top-0 left-0 mt-1 ml-1 h-full w-full rounded bg-black"></span>
-    <span class="fold-bold relative inline-block h-full w-full rounded border-2 border-black bg-white whitespace-nowrap px-1 lg:px-6 py-3 text-base font-bold text-black transition duration-100 hover:bg-tirtry hover:text-gray-900">
+    <Link to="/contact" className="relative" >
+    <span className="absolute top-0 left-0 mt-1 ml-1 h-full w-full rounded bg-black"></span>
+    <span className="fold-bold relative inline-block h-full w-full rounded border-2 border-black bg-white whitespace-nowrap px-1 lg:px-6 py-3 text-base font-bold text-black transition duration-100 hover:bg-tirtry hover:text-gray-900">
     
-     {isClient && (
-              <PopupButton
-                url="https://calendly.com/infolatchltd/30min"
-                rootElement={document.body}
-                text="Book A Call"
-              />
-            )}
+   Book A call
     </span>
-    </div>
+    </Link>
   );
 };
 
